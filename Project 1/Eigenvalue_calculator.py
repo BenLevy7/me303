414dd595c7de657493026c977cc470dff10c44b7
BenLevy7 <76595224+BenLevy7@users.noreply.github.com>
2023-03-15 21:58:15 -0400

    added comments

--- Project 1/Eigenvalue_calculator.py
+++ Project 1/Eigenvalue_calculator.py
@@ -17,24 +17,25 @@ def Eigenvalue (mass, a, b, front_stiff, rear_stiff, yaw_inertia,):
     
 
     while True: 
+        #Matrix entries
         d = ((-(front_stiff + rear_stiff))/(mass*velocity)) 
         e = ((-(a*front_stiff-b*rear_stiff))/(mass*velocity))-velocity
         f = ((-(a*front_stiff-b*rear_stiff))/(yaw_inertia*velocity))
         g = ((-(a**2*front_stiff+b**2*rear_stiff)/(yaw_inertia*velocity)))
         
-        matrix = np.array([[d, e], [f, g]])
-        eigenvalues, eigenvectors = eig(matrix)
+        matrix = np.array([[d, e], [f, g]]) #Establishing matrix for numpy
+        eigenvalues, eigenvectors = eig(matrix) #Calculating the Eigenvalues
         
         
-        if ((eigenvalues > 0).any()):
+        if ((eigenvalues > 0).any()): #if one or both of the eigenvalues are positive, stop the loop and return the velocity corresponding with the previous velocity (to ensure that max speed matches 2 negative eigenvalues)
             break
         velocity += 0.1
         print(eigenvalues)
 
-    return velocity
+    return velocity #Return the max velocity
 
 velocity = Eigenvalue(1400, 1.14, 1.33, 25000, 21000, 2420)
-print(velocity*3.6)
+print(velocity*3.6) #convert max velocity in m/s to km/h
 
 
 
